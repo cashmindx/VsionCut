140075e9f14b9eae6c5d0c5a092d65b202069130
CASHMIND X <theknightmentor@gmail.com>
2025-07-19 13:12:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 // vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist',      // Output folder after build
+    emptyOutDir: true,   // Clear dist before build (recommended)
   },
 });
